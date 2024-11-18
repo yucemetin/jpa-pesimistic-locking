d9b2414332ac7bd8d11ee5a01e58e43939c88640
yucemetin <red.banana988@gmail.com>
2024-11-19 00:12:40 +0300

    Initial commit

---
+++ src/main/java/com/example/pesimisticlocking/PesimisticlockingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pesimisticlocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesimisticlockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesimisticlockingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pesimisticlocking/PesimisticlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pesimisticlocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesimisticlockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
